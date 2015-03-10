2b735e465cb4b65d544fec2f21d5c262b4d81ef6
jhoffma <juliusz.hoffmann@gmail.com>
2015-03-10 15:20:06 +0000

    timer sends messages

--- timer.py
+++ timer.py
@@ -6,25 +6,32 @@ import socket
 import paho.mqtt.client as mqtt
 import json
 
-topic = "WaaS/personDetector"
+changesTopic = "WaaS/personDetector"
+statusTopic = "WaaS/user"
 timerTopic = "WaaS/timer"
 
-cabins = { '1': {'busy': False, 'time': time.time()}, '2': {'busy': False, 'time': time.time()}, '3': {'busy': False, 'time': time.time()}, '4': {'busy': False, 'time': time.time()}}
+cabins = { '2': {'busy': False, 'beginTime': int(time.time()), 'endTime': int(time.time())}, '3': {'busy': False, 'beginTime': int(time.time()), 'endTime': int(time.time())}}
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe(topic)
+    print("Connected with result code " + str(rc))
+    client.subscribe(changesTopic)
+    client.subscribe(statusTopic)
 
 def on_message(client, userdata, msg):
     global cabins
-    print(msg.payload)
+    #print(msg.payload)
     jsonObj = json.loads(msg.payload)
     cabinId = str(jsonObj["id"])
-    if (jsonObj["action"] == "free"):
-        cabins[cabinId]["busy"] = False
-    if (jsonObj["action"] == "busy"):
-        cabins[cabinId]["busy"] = True
-	cabins[cabinId]["time"] = time.time()
+    if msg.topic == statusTopic:
+        cabins[cabinId]["busy"] = jsonObj["action"] == "busy"
+    elif msg.topic == changesTopic:
+        t = int(time.time())
+        if (jsonObj["action"] == "free"):
+            cabins[cabinId]["busy"] = False
+            cabins[cabinId]["endTime"] = t
+        if (jsonObj["action"] == "busy"):
+            cabins[cabinId]["busy"] = True
+	    cabins[cabinId]["beginTime"] = t
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -38,30 +45,45 @@ setText("-" * 16 + "\n" + "-" * 16)
 
 setRGB(0,255,255)
 
-def process(id, isBusy, startTime, row):
-    runSeconds = int(time.time() - startTime)
+def process(id, status, row):
+    print str(id) + ": " + str(status)
+
+    isBusy = status['busy']
+    beginTime = status['beginTime']
+    endTime = status['endTime']
+
+    runSeconds = int(time.time() - beginTime)
     
     data = {}
     data['id'] = id
     data['busy'] = isBusy
     data['hasJustChanged'] = runSeconds == 0
-    data['runSeconds'] = runSeconds
+    if not isBusy:
+        data['runSeconds'] = int(time.time()) - endTime
+    else:
+        data['runSeconds'] = runSeconds
+    data['gender'] = 'm'
 
-    print "id: " + str(id) + ", isBusy: " + str(isBusy) + ", row: " + str(row)
+    #print "id: " + str(id) + ", isBusy: " + str(isBusy) + ", row: " + str(row)
 
     if not isBusy:
-        pass
-        #setTextInRow("Wolne,zapraszam!", row)
+        #pass
+	runSeconds = endTime - beginTime
+	seconds = int(runSeconds) % 60
+	minutes = int(runSeconds / 60)
+        setTextInRow("Srano: " + str(minutes) + "m " + str(seconds) + "s ", row)
     else:
         seconds = int(runSeconds) % 60
         minutes = int(runSeconds / 60)
         setTextInRow("Srasz: " + str(minutes) + "m " + str(seconds) + "s ", row)
+
     client.publish(timerTopic, json.dumps(data))
+
 
 try:
     while(True):
-        process(2, cabins['2']['busy'], cabins['2']['time'], 0) 
-        process(3, cabins['3']['busy'], cabins['3']['time'], 1)
+        process(2, cabins['2'], 0) 
+        process(3, cabins['3'], 1)
 
         if cabins['2']["busy"] and cabins['3']["busy"]:
             setRGB(255, 0, 0)
